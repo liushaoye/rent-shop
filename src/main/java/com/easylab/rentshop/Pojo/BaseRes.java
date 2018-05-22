1547cc8f6aa25b8f24f494721597beaaa161907a
Terry <2219569347@qq.com>
2018-05-22 19:43:19 +0800

    first commit

---
+++ src/main/java/com/easylab/rentshop/Pojo/BaseRes.java
@@ -0,0 +1,18 @@
+package com.easylab.rentshop.Pojo;
+
+/***
+ * *
+ * 名称：     BaseRes.
+ * 作者：     Terry Tan
+ * 创建时间：  on 2018/5/16.
+ * 说明：     
+ * *
+ ***/
+
+public class BaseRes {
+
+    public int code;
+    public String msg;
+    public Object data;
+
+}
---
+++ src/main/java/com/easylab/rentshop/RentShopApplication.java
@@ -0,0 +1,12 @@
+package com.easylab.rentshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentShopApplication.class, args);
+    }
+}
